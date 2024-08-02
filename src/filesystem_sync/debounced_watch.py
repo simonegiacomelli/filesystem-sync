027f9c1b0ba2d40771261c3e9ee1aecf49293ac4
Simone <simone.giacomelli@gmail.com>
2024-08-02 12:45:21 +0100

    watch - development

--- src/filesystem_sync/debounced_watch.py
+++ src/filesystem_sync/debounced_watch.py
@@ -1,10 +1,17 @@
+import json
+import sys
+import tempfile
+import threading
+from datetime import timedelta
 from pathlib import Path
+from time import sleep
 from typing import Callable
 
 from watchdog.events import FileSystemEventHandler, FileSystemEvent
 from watchdog.observers import Observer
 
 from filesystem_sync.debouncer import Debouncer
+from filesystem_sync.debouncer_thread import DebouncerThread
 
 
 class DebouncedObserver:
@@ -34,3 +41,27 @@ class _Handler(FileSystemEventHandler):
         self._callback(event)
 
 
+def main():
+    def new_tmp_path() -> Path:
+        return Path(tempfile.mkdtemp(prefix='debounce-harness'))
+
+    debouncer = Debouncer(timedelta(milliseconds=100))
+
+    def print_events(events):
+        print('=' * 20 + f' current thread name={threading.current_thread().name}')
+        print(f'len={len(events)} events={events}')
+        j = json.dumps(events)
+        print(j)
+
+    DebouncerThread(debouncer, print_events)
+
+    tmp_path = sys.argv[1] if len(sys.argv) > 1 else new_tmp_path()
+    print(f'Watching {tmp_path}')
+    DebouncedObserver(tmp_path, debouncer).watch_directory()
+
+    while True:
+        sleep(1)
+
+
+if __name__ == '__main__':
+    main()
