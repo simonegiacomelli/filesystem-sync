e4a539f40f9b8a539f992bf44962e5b6b09ff99e
Simone <simone.giacomelli@gmail.com>
2024-08-03 11:45:13 +0100

    watch - development

--- tests/activity_monitor.py
+++ tests/activity_monitor.py
@@ -1,13 +1,7 @@
 from __future__ import annotations
 
-from datetime import datetime, timedelta
+from datetime import timedelta, datetime
 from typing import Callable
-
-import pytest
-
-from tests.time_mock import TimeMock
-
-_time_mock = TimeMock()
 
 
 class ActivityMonitor:
@@ -36,24 +30,3 @@ class ActivityMonitor:
 
     def touch(self):
         self._last_touch = self._time_func()
-
-
-@pytest.fixture
-def target():
-    _time_mock.reset()
-    return ActivityMonitor(timedelta(milliseconds=20), time_func=_time_mock)
-
-
-def test_activity_monitor(target):
-    assert target.at_rest()
-
-
-def test_touch__change_at_rest(target):
-    target.touch()
-    assert not target.at_rest()
-
-
-def test_touch_after_time_window(target):
-    target.touch()
-    _time_mock.advance(timedelta(milliseconds=21))
-    assert target.at_rest()
---
+++ tests/activity_monitor_test.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from datetime import timedelta
+
+import pytest
+
+from tests.activity_monitor import ActivityMonitor
+from tests.time_mock import TimeMock
+
+_time_mock = TimeMock()
+
+
+@pytest.fixture
+def target():
+    _time_mock.reset()
+    return ActivityMonitor(timedelta(milliseconds=20), time_func=_time_mock)
+
+
+def test_activity_monitor(target):
+    assert target.at_rest()
+
+
+def test_touch__change_at_rest(target):
+    target.touch()
+    assert not target.at_rest()
+
+
+def test_touch_after_time_window(target):
+    target.touch()
+    _time_mock.advance(timedelta(milliseconds=21))
+    assert target.at_rest()
